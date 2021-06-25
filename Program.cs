acec9c6ff5b630118529c8e945de613414635358
tizirpoli <tizirpoli@gmail.com>
2021-06-25 18:01:42 -0300

    Cria Projeto

---
+++ Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static SerieRepositorio repositorio = new SerieRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                case "1":
+                ListarSerie();
+                break;
+                
+                case "2":
+                InserirSerie();
+                break;
+
+                case "3":
+                AtualizarSerie();
+                break;
+
+                case "4":
+                ExcluirSerie();
+                break;
+
+                case "5":
+                VisualizarSerie();
+                break;
+
+                case "C":
+                Console.Clear();
+                break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            opcaoUsuario = ObterOpcaoUsuario();
+        }
+
+        Console.WriteLine("Agradecemos por utilizar nossos serviços.");
+        Console.ReadLine();
+    }
+
+        private static void VisualizarSerie()
+        {
+             Console.Write("Digite o ID da Série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            var serie = repositorio.RetornaPorId(indiceSerie);
+
+            Console.WriteLine(serie);
+        }
+
+        private static void ExcluirSerie()
+        {
+            Console.Write("Digite o ID da Série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            repositorio.Exclui(indiceSerie);
+        }
+
+        private static void AtualizarSerie()
+        {
+            Console.WriteLine("Digite o ID da Série: ");
+            int indiceSerie = int.Parse(Console.ReadLine());
+
+            foreach( int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.WriteLine("Escolha o gênero dentre as opções acima:");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Título da série:");
+            string entradaTitulo = Console.ReadLine();
+            
+            Console.WriteLine("Digite o Ano de Início da série:");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Descrição da série:");
+            string entradaDescrição = Console.ReadLine();
+            
+            Serie atualizaSerie = new Serie(id: indiceSerie,
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescrição);
+            repositorio.Atualiza(indiceSerie, atualizaSerie);
+        }
+
+        private static void ListarSerie()
+        {
+            Console.WriteLine("Listar Séries");
+
+            var lista = repositorio.Lista();
+
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("Nenuma série cadastrada :(");
+                return;
+            }
+
+            foreach (var serie in lista)
+            {
+                var excluido = serie.retornaExcluido();
+                
+                Console.WriteLine("#ID {0}: - {1} {2}", serie.retornaID(), serie.retornaTitulo(), (excluido ? "*Excluído*" : ""));
+            }
+        }
+             private static void InserirSerie()
+        {
+            Console.WriteLine("Insira a Nova Série");
+            foreach (int i in Enum.GetValues(typeof(Genero)))
+            {
+                Console.WriteLine("{0} - {1}", i, Enum.GetName(typeof(Genero), i));
+            }
+            Console.WriteLine("Escolha o gênero dentre as opções acima:");
+            int entradaGenero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Título da série:");
+            string entradaTitulo = Console.ReadLine();
+            
+            Console.WriteLine("Digite o Ano de Início da série:");
+            int entradaAno = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Descrição da série:");
+            string entradaDescrição = Console.ReadLine();
+            
+            Serie novaSerie = new Serie(id: repositorio.ProximoID(),
+                                        genero: (Genero)entradaGenero,
+                                        titulo: entradaTitulo,
+                                        ano: entradaAno,
+                                        descricao: entradaDescrição);
+            repositorio.Insere(novaSerie);
+        }
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Séries a seu dispor!");
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine();
+            Console.WriteLine("1- Listar Séries");
+            Console.WriteLine("2- Inserir nova série");
+            Console.WriteLine("3- Atualizar série");
+            Console.WriteLine("4- Excluir série");
+            Console.WriteLine("5- Visualizar série");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+#	Exercício - DIO
+
+## Cadastro de Séries :tv:	
+
+Criação de algoritmo simples de cadastro de séries para praticar orientação a objetos. Nesse projeto utilizando: Como pensar orientado a objetos, como modelar o seu domínio, como utilizar recursos de coleção para salvar seus dados em memória.
+
+Instrutor: Eliézer Zarpelão
